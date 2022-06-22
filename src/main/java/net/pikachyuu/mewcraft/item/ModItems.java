34d3e1e15af1e6da5d5192bebc14b9534003477f
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-06-22 12:06:28 -0700

    Added items to item group

--- src/main/java/net/pikachyuu/mewcraft/item/ModItems.java
+++ src/main/java/net/pikachyuu/mewcraft/item/ModItems.java
@@ -10,13 +10,13 @@ import net.pikachyuu.mewcraft.Mewcraft;
 public class ModItems {
 
     public static final Item SAKURA_INGOT = registerItem("sakura_ingot",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MEWCRAFT)));
 
     public static final Item MEW_INGOT = registerItem("mew_ingot",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MEWCRAFT)));
 
     public static final Item MEWLINGS_TAIL = registerItem("mewlings_tail",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MEWCRAFT)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Mewcraft.MOD_ID, name), item);
     }
