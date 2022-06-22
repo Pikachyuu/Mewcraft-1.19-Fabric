a265c531954abf81f1fc800919c55277ad829bb2
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-06-22 12:06:00 -0700

    Created custom item group for mod.

---
+++ src/main/java/net/pikachyuu/mewcraft/item/ModItemGroup.java
@@ -0,0 +1,13 @@
+package net.pikachyuu.mewcraft.item;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.pikachyuu.mewcraft.Mewcraft;
+
+public class ModItemGroup {
+    public static final ItemGroup MEWCRAFT = FabricItemGroupBuilder.build(new Identifier(Mewcraft.MOD_ID, "mewcraft"),
+            () -> new ItemStack(ModItems.MEWLINGS_TAIL));
+}
