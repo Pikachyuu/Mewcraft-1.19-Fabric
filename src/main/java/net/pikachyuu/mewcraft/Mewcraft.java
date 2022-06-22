e01a7efb5908e2b5d99894207014a0652406b231
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-06-21 22:29:55 -0700

    Added Items
    It's some simple items and a very simple recipe, nothing game changing.

--- src/main/java/net/pikachyuu/mewcraft/Mewcraft.java
+++ src/main/java/net/pikachyuu/mewcraft/Mewcraft.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Mewcraft implements ModInitializer {
-	public static final String MOD_ID = "fecesmod";
+	public static final String MOD_ID = "mewcraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
--- src/main/java/net/pikachyuu/mewcraft/item/ModItems.java
+++ src/main/java/net/pikachyuu/mewcraft/item/ModItems.java
@@ -1,11 +1,22 @@
 package net.pikachyuu.mewcraft.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.pikachyuu.mewcraft.Mewcraft;
 
 public class ModItems {
+
+    public static final Item SAKURA_INGOT = registerItem("sakura_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MEW_INGOT = registerItem("mew_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MEWLINGS_TAIL = registerItem("mewlings_tail",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Mewcraft.MOD_ID, name), item);
     }
