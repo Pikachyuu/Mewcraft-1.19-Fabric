a23aa089b10145e17584c49484423da6c0942fc5
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-06-22 18:35:10 -0700

    Setup all necessary classes so textures can be applied to blocks
    It's not super complex or anything, just a lot of .json files

--- src/main/java/net/pikachyuu/mewcraft/block/ModBlocks.java
+++ src/main/java/net/pikachyuu/mewcraft/block/ModBlocks.java
@@ -4,10 +4,12 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.pikachyuu.mewcraft.Mewcraft;
 import net.pikachyuu.mewcraft.item.ModItemGroup;
@@ -15,7 +17,15 @@ import net.pikachyuu.mewcraft.item.ModItemGroup;
 public class ModBlocks {
 
     public static final Block MEWNITE_BLOCK = registerBlock("mewnite_block",
-            new Block(FabricBlockSettings.of(Material.METAL)), ModItemGroup.MEWCRAFT);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(4).requiresTool()), ModItemGroup.MEWCRAFT);
+
+    public static final Block SAKURA_ORE = registerBlock("sakura_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4).requiresTool(),
+            UniformIntProvider.create(8, 12)), ModItemGroup.MEWCRAFT);
+
+    public static final Block DEEPSLATE_SAKURA_ORE = registerBlock("deepslate_sakura_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4).requiresTool(),
+                    UniformIntProvider.create(8, 12)), ModItemGroup.MEWCRAFT);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
