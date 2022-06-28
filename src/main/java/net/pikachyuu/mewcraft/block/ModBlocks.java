9137c3fa657dd3eb43d8c8b4c9b53607b1f87069
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-06-28 02:31:38 -0700

    Added some wood blocks. That's it.
    Yeah. Ik. It's 35 files. So what. It's for wood. Obviously

--- src/main/java/net/pikachyuu/mewcraft/block/ModBlocks.java
+++ src/main/java/net/pikachyuu/mewcraft/block/ModBlocks.java
@@ -2,10 +2,7 @@ package net.pikachyuu.mewcraft.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.block.OreBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -17,6 +14,12 @@ import net.pikachyuu.mewcraft.Mewcraft;
 import net.pikachyuu.mewcraft.item.ModItemGroup;
 
 public class ModBlocks {
+
+    public static final Block TEST_BLOCK_1 = registerBlock("test_block_1",
+            new Block(FabricBlockSettings.of(Material.BARRIER)), ModItemGroup.MEWCRAFT);
+
+    public static final Block TEST_BLOCK_2 = registerBlock("test_block_2",
+            new Block(FabricBlockSettings.of(Material.BARRIER)), ModItemGroup.MEWCRAFT);
 
     public static final Block SAKURA_BLOCK = registerBlock("sakura_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(3).requiresTool()), ModItemGroup.MEWCRAFT);
@@ -32,11 +35,22 @@ public class ModBlocks {
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4).requiresTool(),
                     UniformIntProvider.create(8, 12)), ModItemGroup.MEWCRAFT);
 
-    public static final Block TEST_BLOCK_1 = registerBlock("test_block_1",
-            new Block(FabricBlockSettings.of(Material.BARRIER)), ModItemGroup.MEWCRAFT);
+    public static final Block SAKURA_LOG = registerBlock("sakura_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4.0f).requiresTool()), ModItemGroup.MEWCRAFT);
 
-    public static final Block TEST_BLOCK_2 = registerBlock("test_block_2",
-            new Block(FabricBlockSettings.of(Material.BARRIER)), ModItemGroup.MEWCRAFT);
+    public static final Block SAKURA_WOOD = registerBlock("sakura_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(4.0f).requiresTool()), ModItemGroup.MEWCRAFT);
+
+    public static final Block STRIPPED_SAKURA_LOG = registerBlock("stripped_sakura_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).strength(4.0f).requiresTool()), ModItemGroup.MEWCRAFT);
+
+    public static final Block STRIPPED_SAKURA_WOOD = registerBlock("stripped_sakura_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).strength(4.0f).requiresTool()), ModItemGroup.MEWCRAFT);
+
+    //May have to add some sort of tag to the wood
+    public static final Block SAKURA_PLANKS = registerBlock("sakura_planks",
+            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).strength(4.0f).requiresTool()), ModItemGroup.MEWCRAFT);
+
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
--- src/main/java/net/pikachyuu/mewcraft/util/ModRegistries.java
+++ src/main/java/net/pikachyuu/mewcraft/util/ModRegistries.java
@@ -1,6 +1,9 @@
 package net.pikachyuu.mewcraft.util;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+import net.pikachyuu.mewcraft.block.ModBlocks;
 import net.pikachyuu.mewcraft.entity.ModEntities;
 import net.pikachyuu.mewcraft.entity.custom.MewlingEntity;
 
@@ -11,5 +14,20 @@ public class ModRegistries {
 
     private static void registerAttributes() {
         FabricDefaultAttributeRegistry.register(ModEntities.MEWLING, MewlingEntity.setAttributes());
+    }
+
+    private static void registerFlammableBlock() {
+        FlammableBlockRegistry instance = FlammableBlockRegistry.getDefaultInstance();
+
+        instance.add(ModBlocks.SAKURA_LOG, 5, 20);
+        instance.add(ModBlocks.SAKURA_WOOD, 5, 20);
+        instance.add(ModBlocks.STRIPPED_SAKURA_LOG, 5, 20);
+        instance.add(ModBlocks.STRIPPED_SAKURA_WOOD, 5, 20);
+        instance.add(ModBlocks.SAKURA_PLANKS, 5, 20);
+    }
+
+    private static void registerStrippables() {
+        StrippableBlockRegistry.register(ModBlocks.SAKURA_LOG, ModBlocks.STRIPPED_SAKURA_LOG);
+        StrippableBlockRegistry.register(ModBlocks.SAKURA_WOOD, ModBlocks.STRIPPED_SAKURA_WOOD);
     }
 }
