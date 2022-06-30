e739a42ba7bc4495417c13784601ca273eea36a2
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-06-30 04:35:43 -0700

    Did some messing around with biomes
    Will revisit at a later date

---
+++ src/main/java/net/pikachyuu/mewcraft/world/biome/ModBiomeKeys.java
@@ -0,0 +1,16 @@
+package net.pikachyuu.mewcraft.world.biome;
+
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
+import net.pikachyuu.mewcraft.Mewcraft;
+
+public class ModBiomeKeys {
+    public static final RegistryKey<Biome> SAKURA_FOREST = register("sakura_forest");
+
+    private static RegistryKey<Biome> register(String name)
+    {
+        return RegistryKey.of(Registry.BIOME_KEY, new Identifier(Mewcraft.MOD_ID, name));
+    }
+}
