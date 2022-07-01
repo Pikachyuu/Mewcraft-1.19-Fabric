e0c8aa867dd772d38cc2449d111274ee48cbfed6
Pikachyuu <75066008+Pikachyuu@users.noreply.github.com>
2022-07-01 03:13:55 -0700

    Temporarily commented out or removed instances of dimensions.

---
+++ src/main/java/net/pikachyuu/mewcraft/world/biome/ModBiomeSelectors.java
@@ -0,0 +1,14 @@
+package net.pikachyuu.mewcraft.world.biome;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext;
+import net.minecraft.world.dimension.DimensionOptions;
+//import net.pikachyuu.mewcraft.world.dimension.ModDimensions;
+
+import java.util.function.Predicate;
+/*
+public class ModBiomeSelectors {
+    public static Predicate<BiomeSelectionContext> foundInMewdim() {
+        return context -> context.canGenerateIn(ModDimensions.MEWDIM);
+    }
+}
+ */
